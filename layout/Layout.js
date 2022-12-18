7e247c9d05f1168f358525ad749cff4b02d50ebd
jorneylopez <jorney95@live.com>
2022-12-18 14:56:57 -0600

    Added modal

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,25 @@
 import Head from "next/head";
+import Modal from "react-modal";
 import Sidebar from "../components/Sidebar";
+import ModalProducto from "../components/ModalProducto";
+import useQuiosco from "../hooks/useQuiosco";
+
+const modalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#__next");
 
 export default function Layout({ children, pagina }) {
+  const { modal } = useQuiosco();
+
   return (
     <>
       <Head>
@@ -15,12 +33,14 @@ export default function Layout({ children, pagina }) {
         </aside>
 
         <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-          <div className="p-10">
-            {children}
-          </div>
+          <div className="p-10">{children}</div>
         </main>
       </div>
-
+      {modal && (
+        <Modal isOpen={modal} style={modalStyles}>
+          <ModalProducto />
+        </Modal>
+      )}
     </>
   );
 }
