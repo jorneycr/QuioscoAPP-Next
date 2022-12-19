0f7261b9fac59e84889fd10488327f2af571d0f4
jorneylopez <jorney95@live.com>
2022-12-18 23:06:00 -0600

    Added toast

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, createContext } from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const QuioscoContext = createContext();
 
@@ -32,8 +33,11 @@ const QuioscoProvider = ({ children }) => {
         if (pedido.some(productoState => productoState.id === producto.id)) {
             const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState);
             setPedido(pedidoActualizado);
+            toast.success('Guardado Correctamenta');
+
         } else {
             setPedido([...pedido, producto]);
+            toast.success('Agregado al Pedido');
         }
         setModal(false);
     }
