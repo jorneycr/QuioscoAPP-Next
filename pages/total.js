a949453120ba813d9c70c838f7cb78e79a84ec68
jorneylopez <jorney95@live.com>
2022-12-18 23:23:45 -0600

    Added pasos menu

--- pages/total.js
+++ pages/total.js
@@ -1,5 +1,10 @@
-
+import Layout from "../layout/Layout";
 
 export default function Total() {
-    return <h1>Total</h1>;
+  return (
+    <Layout pagina="Total">
+      <h1 className="text-4xl font-black ">Total</h1>
+      <p className="text-2xl my-10">Confirma tu pedido</p>
+    </Layout>
+  );
 }
