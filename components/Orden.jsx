fa64ed95d7c4ab28d26a55961cc2a01c04c776d0
jorneylopez <jorney95@live.com>
2022-12-23 01:12:37 -0600

    Showed ordens

--- components/Orden.jsx
+++ components/Orden.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import { formatearDinero } from "../helpers";
 
 const Orden = ({ orden }) => {
     const { id, nombre, total, pedido } = orden;
@@ -7,7 +9,27 @@ const Orden = ({ orden }) => {
             <h3 className="text-2xl font-bold">Orden: {id}</h3>
             <p className="text-lg my-10 font-bold">Cliente: {nombre}</p>
             <div className="">
-
+                {pedido.map(plato => (
+                    <div key={plato.id} className="py-3 flex border-b last-of-type:border-0 items-center">
+                        <div className="w-32">
+                            <Image
+                                width={400}
+                                height={500}
+                                src={`/assets/img/${plato.imagen}.jpg`}
+                                alt={`Imagen Plato ${plato.nombre}`}
+                            />
+                        </div>
+                        <div className="p-5 space-y2">
+                            <h4 className="text-xl font-bold text-amber-500">{plato.nombre}</h4>
+                            <p className="text-lg font-bold">Cantidad: {plato.cantidad}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="md:flex md:items-center md:justify-between my-10">
+                <p className="mt-5 font-black text-4xl text-amber-500">
+                    Total a pagar: {formatearDinero(total)}
+                </p>
             </div>
         </div>
     )
