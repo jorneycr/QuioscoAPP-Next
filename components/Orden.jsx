9ac61c4555a9e36595c9cb47c291037c8fd317c2
jorneylopez <jorney95@live.com>
2022-12-23 00:58:01 -0600

    Added orden component

--- components/Orden.jsx
+++ components/Orden.jsx
@@ -6,6 +6,9 @@ const Orden = ({ orden }) => {
         <div className=" border p-10 space-y-5">
             <h3 className="text-2xl font-bold">Orden: {id}</h3>
             <p className="text-lg my-10 font-bold">Cliente: {nombre}</p>
+            <div className="">
+
+            </div>
         </div>
     )
 }
